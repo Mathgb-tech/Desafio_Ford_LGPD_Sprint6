4222e159bb39b406dc9025e82bcdb08600bc42af
MATHEUS LEITE GUBERT <232.976614@universidadesenaicimatec.edu.br>
2026-05-27 16:52:11 -0300

    incluindo checkbox

--- js/form.js
+++ js/form.js
@@ -2,14 +2,14 @@
 //class contato
 
 class contato {
-    constructor(nome, sobrenome, email, telefone,  contato, tipo_contato, mensagem) {
-            this.nome = nome;
-            this.sobrenome = sobrenome;
-            this.email = email;
-            this.telefone = telefone;
-            this.contato = contato;
-            this.tipo_contato = tipo_contato;
-            this.mensagem = mensagem;
+    constructor(nome, sobrenome, email, telefone, contato, tipo_contato, mensagem) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.telefone = telefone;
+        this.contato = contato;
+        this.tipo_contato = tipo_contato;
+        this.mensagem = mensagem;
     }
 }
 
@@ -23,20 +23,30 @@ function Post(form) {
         form.elements.namedItem("tipo-contato").value,
         form.elements.namedItem("mensagem").value);
 
+
     Enviar(data);
+    return false;
 }
 
 function Enviar(dados) {
     const forms = document.querySelector("#formulario");
-    document.addEventListener("submit", (e) => {
-        e.preventDefault();
-        var nome = document.getElementById("nomeid");
+    var nome = document.getElementById("nomeid");
 
-        if (nome.value != "") {
-            alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-        }   
-        forms.reset();
-        console.log(dados)
-        console.log(`nome: ${dados.nome} \n sobrenome: ${dados.sobrenome} \n email: ${dados.email} \n telefone: ${dados.telefone} \n contato: ${dados.contato} \n Tipo do contato: ${dados.tipo_contato} \n Mensagem: ${dados.mensagem}`);
-    }, { once: true});
-}
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+    forms.reset();
+    document.getElementById("btn").disabled = true;
+    console.log(dados);
+    console.log(`nome: ${dados.nome} \n sobrenome: ${dados.sobrenome} \n email: ${dados.email} \n telefone: ${dados.telefone} \n contato: ${dados.contato} \n Tipo do contato: ${dados.tipo_contato} \n Mensagem: ${dados.mensagem}`);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.getElementById("checado");
+    checkbox.onchange = (event) => {
+        const checkbox = event.target;
+        const btn = document.getElementById("btn");
+
+        checkbox.checked ? btn.disabled = !checkbox.checked : btn.disabled = true;
+    };
+});
